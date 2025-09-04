03f7f03166701238ed71ff4469f4ce30f4a4fdbc
Shadrack <iamshadrack10@gmail.com>
2025-09-04 12:31:15 +0000

    backend

--- Backend/flask_project/models.py
+++ Backend/flask_project/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy 
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from datetime import datetime
+from flask import url_for
 
 db = SQLAlchemy() 
 class User(db.Model): 
@@ -10,10 +11,13 @@ class User(db.Model):
     is_verified = db.Column(db.Boolean,default = False) 
     password = db.Column(db.String(150),nullable = False)
     profile_image = db.Column(db.String(300))
-    general_settings = db.relationship("GeneralSettings",backref ="user",uselist = False,lazy=True)
-    theme_settings = db.relationship("ThemeSettings",backref = "user",uselist = False,lazy=True)
-    passage_settings = db.relationship("CustomPassageSettings",backref="user",uselist = False,lazy=True) 
-    reset_tokens = db.relationship("ResetToken",backref="user", lazy = True)
+    general_settings = db.relationship("GeneralSettings",backref ="user",uselist = False,lazy="select")
+    theme_settings = db.relationship("ThemeSettings",backref = "user",uselist = False,lazy="select")
+    passage_settings = db.relationship("CustomPassageSettings",backref="user",uselist = False,lazy="select") 
+    reset_tokens = db.relationship("ResetToken",backref="user", lazy = "select")
+    history = db.relationship("History",backref=db.backref("user",cascade="all,delete-orphan"),lazy="select")
+    text_snippet = db.relationship("TextSnippets",backref=db.backref("user",cascade="all, delete-orphan"),lazy="select")
+    custom_text = db.relationship("CustomTexts",backref=db.backref("user",cascade="all,delete-orphan"),lazy="select")
 
     def set_password(self, password):
          self.password = generate_password_hash(password)
@@ -26,7 +30,7 @@ class User(db.Model):
         return {
             "id":self.id,
             "userName":self.user_name,
-            "profile_image":self.profile_image,
+            "profile_image":url_for("uploaded_file",filename=self.profile_image,_external=True) if self.profile_image else None,
             "email":self.email
         }
 
@@ -39,7 +43,6 @@ class GeneralSettings(db.Model):
     difficulty = db.Column(db.String(20), default="medium")
     auto_stat_text = db.Column(db.Boolean,default = False)
     enable_sound_effect = db.Column(db.Boolean,default = False)
-
 
 
 class ThemeSettings(db.Model):
@@ -63,3 +66,64 @@ class ResetToken(db.Model):
     token = db.Column(db.String(300),nullable = False,unique=True)
     expires_at = db.Column(db.DateTime,nullable = False)
     used = db.Column(db.Boolean,default = False)
+
+class History(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    wpm = db.Column(db.Float,nullable=False)
+    accuracy = db.Column(db.Float,nullable=False)
+    score = db.Column(db.Float,nullable=False)
+    fixed_time = db.Column(db.Float,default=120.0)
+    stopped_time = db.Column(db.Float,nullable=False)
+    created_at = db.Column(db.DateTime,default = datetime.utcnow)
+    completed = db.Column(db.Boolean,default=False)
+
+    def to_diction(self):
+        return {
+            "profileImage":url_for("uploaded_file",filename=self.user.profile_image,_external=True) if self.user.profile_image else None,
+            "userId":self.user_id,
+            "wpm":self.wpm,
+            "accuracy":self.accuracy,
+            "score":self.score,
+            "createdAt":self.created_at.isoformat(),
+            "userName":self.user.user_name
+        } 
+
+class TextSnippets(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    difficulty = db.Column(db.String(15), default = "Medium")
+   
+    def to_john(self):
+        return {
+            "content":self.content, 
+            "difficulty":self.difficulty
+        }
+
+class Duel(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    player1_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    player2_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    text_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    start_time = db.Column(db.DateTime,nullable=False)
+    end_time = db.Column(db.DateTime,nullable=False)
+    winner_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    status = db.Column(db.String(20),default="Pending")
+
+class CustomTexts(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    creator = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    title = db.Column(db.String(40),nullable = False)
+    text_content = db.Column(db.String(500),nullable=False)
+    difficulty = db.Column(db.String(10),default="Medium",nullable=False)
+    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def to_shark(self):
+        return {
+            "user":self.user.user_name,
+            "content":self.text_content,
+            "difficulty":self.difficulty,
+            "createdAt":self.created_at.isoformat()
+        } 
+
