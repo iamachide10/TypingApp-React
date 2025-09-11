6c4435df42f6e5e0211025733263d7b191c06258
john <iamachide10@gmail.com>
2025-09-11 12:31:33 +0000

    test

--- Backend/flask_project/main.py
+++ Backend/flask_project/main.py
@@ -754,6 +754,8 @@ def get_history():
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
+        print("✅ Database tables created/verified")
+        print("Tables in DB:", db.metadata.tables.keys())  #
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port= port   ,debug=False)
  
